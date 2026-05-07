f59def686f8428a09511c9761eac3dae0f1bc25b
Vantage jay <jesuferanmiojatunwase@gmail.com>
2026-05-07 06:02:48 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
+import Login from './pages/login'
 
 function App() {
   return (
