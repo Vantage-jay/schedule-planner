675e9fdb306a19645d40c6bca4ff52e38b696f6c
Vantage jay <jesuferanmiojatunwase@gmail.com>
2026-05-07 06:31:22 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/login'
 
+
+
 function App() {
   return (
     <div className="min-h-screen bg-[#0f0f11]">
