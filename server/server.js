fc1945a54a12c574f048fffbe3069617c1929e99
Vantage jay <jesuferanmiojatunwase@gmail.com>
2026-05-07 06:26:51 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,6 +30,10 @@ app.get('*', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+module.exports = app;
